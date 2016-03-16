3de4983f7d45477272a5342dd2413bc130e88d4b
Alecsandr86 <alecsandrcherneckiy@gmail.com>
2016-03-16 20:46:09 +0200

    first-commit

---
+++ src/js/partials/helper.js
@@ -0,0 +1,21 @@
+//Так приятней :)
+window.log = function(param){
+    console.log(param);
+};
+$(document).ready(function(){
+
+
+    //Chrome Smooth Scroll
+    try {
+        $.browserSelector();
+        if($("html").hasClass("chrome")) {
+            $.smoothScroll();
+        }
+    } catch(err) {}
+
+
+
+
+
+
+});
