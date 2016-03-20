70046e28781d3aa47d636760ae9db16130d21218
Alecsandr86 <alecsandrcherneckiy@gmail.com>
2016-03-20 14:23:17 +0200

    catalog-item

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,8 @@
 //= ../../bower_components/jquery/dist/jquery.min.js
 //= ../../bower_components/Swiper/dist/js/swiper.jquery.min.js
 //= ../../bower_components/jquery-form-styler/jquery.formstyler.min.js
+//= ../../bower_components/gilbitron/carouFredSel/jquery.carouFredSel-6.2.1-packed.js
+//= ../../bower_components/tabslet/jquery.tabslet.min.js
 
 /*
     Custom
--- src/js/partials/helper.js
+++ src/js/partials/helper.js
@@ -37,9 +37,72 @@ $(document).ready(function(){
         spaceBetween: 10,
     });
 
+    //////////////////////////////
+
+    $(function() {
+
+        //	wrap all thumbs in a <div> for the 3x3 grid
+        $div = null;
+        $('#thumbs').children().each(function(i) {
+            if ( i % 9 == 0) {
+                $div = $( '<div />' );
+                $div.appendTo( '#thumbs' );
+            }
+            $(this).appendTo( $div );
+            $(this).addClass( 'itm'+i );
+            $(this).click(function() {
+                $('#images').trigger( 'slideTo', [i, 0, true] );
+            });
+        });
+        $('#thumbs img.itm0').addClass( 'selected' );
+
+        //	the big-image carousel
+        $('#images').carouFredSel({
+            direction: 'up',
+            circular: false,
+            infinite: false,
+            width: 310,
+            height: 440,
+            items: 1,
+            auto: false,
+            scroll: {
+                fx: 'directscroll',
+                onBefore: function() {
+                    var pos = $(this).triggerHandler( 'currentPosition' );
+                    $('#thumbs img').removeClass( 'selected' );
+                    $('#thumbs img.itm'+pos).addClass( 'selected' );
+
+                    var page = Math.floor( pos / 9 );
+                    $('#thumbs').trigger( 'slideToPage', page );
+                }
+            }
+        });
+
+        //	the thumbnail-carousel
+        $('#thumbs').carouFredSel({
+            direction: 'up',
+            circular: false,
+            infinite: false,
+            width: 195,
+            height: 400,
+            items: 1,
+            align: false,
+            auto: false,
+        });
+    });
+    
+    
+    
     // \СЛАЙДЕР\
 
-
+    // TABSELET
+    
+        $('.tabs').tabslet({
+            animation: true
+        });
+    
+    
+    // \TABSELET\
 
 
 
